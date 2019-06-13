fcec437c14d3576fbd798d68aed14fed4363a925
Danielle B <danibaxx@hotmail.com>
2019-06-12 18:40:57 -0600

    added function for test in SearchBar.js

---
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SearchBar() {
+  // console.log(props);
+  return (
+    <div className="search-container">
+      <h1>Test Header</h1>
+    </div>
+  );
+}
+
+export default SearchBar;
